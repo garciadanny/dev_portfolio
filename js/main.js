847407d5e7c9b53a3143badc2f525912d5d278fe
Danny Garcia <dannygarcia.me@gmail.com>
2014-07-26 14:46:54 -0600

    scroll to bottom of page

--- js/main.js
+++ js/main.js
@@ -4,12 +4,14 @@ $(document).ready( function() {
         event.preventDefault();
         $(".projects").find(".project-intro").hide();
         $(this).closest(".project").find(".project-description").fadeToggle();
+        $(window).scrollTop( $(document).height() );
     });
 
     $(".back").on("click", function(event) {
         event.preventDefault();
         $(this).closest(".project-description").hide();
         $(".projects").find(".project-intro").fadeToggle();
+        $(window).scrollTop( $(document).height() );
     });
 
 });
