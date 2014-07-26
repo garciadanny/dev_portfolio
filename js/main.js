ddb07539444434b803126a2c16a5be6108febbea
Danny Garcia <dannygarcia.me@gmail.com>
2014-07-26 13:54:57 -0600

    remove scrolling to bottom of page

--- js/main.js
+++ js/main.js
@@ -4,14 +4,12 @@ $(document).ready( function() {
         event.preventDefault();
         $(".projects").find(".project-intro").hide();
         $(this).closest(".project").find(".project-description").fadeToggle();
-        $(window).scrollTop( $(document).height() );
     });
 
     $(".back").on("click", function(event) {
         event.preventDefault();
         $(this).closest(".project-description").hide();
         $(".projects").find(".project-intro").fadeToggle();
-        $(window).scrollTop( $(document).height() );
     });
 
 });
